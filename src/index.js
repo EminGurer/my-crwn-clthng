de144573a6f979a7e1b5216fbdc20544718118e9
Emin Gürer <emin.gurer45@gmail.com>
2022-11-15 22:41:08 +0300

    products context and provider added, applied in shop page

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import App from './App';
+import { ProductsProvider } from './contexts/products.context';
 import { UserProvider } from './contexts/user.context';
+import App from './App';
 
 import './index.scss';
 
@@ -11,9 +12,11 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <UserProvider>
-        <App />
-      </UserProvider>
+      <ProductsProvider>
+        <UserProvider>
+          <App />
+        </UserProvider>
+      </ProductsProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
