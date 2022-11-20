5b35c3622d3407a2f5aaa19076c3a44783861d20
Emin Gürer <emin.gurer45@gmail.com>
2022-11-20 20:53:23 +0300

    fake data replaced with real firestore data, products context changed with categoriesmap context

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 
-import { ProductsProvider } from './contexts/products.context';
+import { CategoriesProvider } from './contexts/categories.context';
 import { UserProvider } from './contexts/user.context';
 import { CartProvider } from './contexts/cart.context';
 import App from './App';
@@ -13,13 +13,13 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <ProductsProvider>
+      <CategoriesProvider>
         <UserProvider>
           <CartProvider>
             <App />
           </CartProvider>
         </UserProvider>
-      </ProductsProvider>
+      </CategoriesProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
