a83c55e5bb1d8639df21c7068c7ad29745df13cb
Emin Gürer <emin.gurer45@gmail.com>
2022-12-05 02:06:13 +0300

    categories context changed with redux toolkit

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -1,12 +1,13 @@
 import { useParams } from 'react-router-dom';
-import { useContext, useState, useEffect, Fragment } from 'react';
-import { CategoriesContext } from '../../contexts/categories.context';
+import { useState, useEffect, Fragment } from 'react';
 import ProductCard from '../../components/product-card/product-card.component';
 import './category.styles.scss';
+import { useSelector } from 'react-redux';
+import { selectCategoriesMap } from '../../redux-store/categories/categories.selector';
 
 const Category = () => {
   const { category } = useParams();
-  const { categoriesMap } = useContext(CategoriesContext);
+  const categoriesMap = useSelector(selectCategoriesMap);
   const [products, setProducts] = useState(categoriesMap[category]);
   useEffect(() => {
     setProducts(categoriesMap[category]);
