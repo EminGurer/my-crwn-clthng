95dd4fdb82c5ead838db3f8095842888ec540265
Emin Gürer <emin.gurer45@gmail.com>
2022-12-13 19:04:00 +0300

    redux-persist is integrated with redux for hydration

--- src/redux-store/store.js
+++ src/redux-store/store.js
@@ -1,10 +1,22 @@
 import { configureStore } from '@reduxjs/toolkit';
 import logger from 'redux-logger';
-
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 import { rootReducer } from './root-reducer';
 
+const persistConfig = {
+  key: 'root',
+  storage: storage,
+  blacklist: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const middlewares = [logger];
+
 export const store = configureStore({
-  reducer: rootReducer,
+  reducer: persistedReducer,
   middleware: middlewares,
 });
+
+export const persistor = persistStore(store);
